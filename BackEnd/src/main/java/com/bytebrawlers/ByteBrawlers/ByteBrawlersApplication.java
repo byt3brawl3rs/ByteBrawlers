1ae249e83d1b156c304ef4a1bc4287142e83e07b
Lukas Tackett <luetack@gmail.com>
2023-10-15 17:30:09 -0400

    Test commit

--- BackEnd/src/main/java/com/bytebrawlers/ByteBrawlers/ByteBrawlersApplication.java
+++ BackEnd/src/main/java/com/bytebrawlers/ByteBrawlers/ByteBrawlersApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ByteBrawlersApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
 		SpringApplication.run(ByteBrawlersApplication.class, args);
 	}
 
