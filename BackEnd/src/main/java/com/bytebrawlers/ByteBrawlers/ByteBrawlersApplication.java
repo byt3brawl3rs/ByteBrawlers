2741a3b8802dcabfea2565331fddd27acd54d581
Lukas <luetack@gmail.com>
2023-10-20 02:28:47 -0400

    Revert "Initial Commit"

---
+++ BackEnd/src/main/java/com/bytebrawlers/ByteBrawlers/ByteBrawlersApplication.java
@@ -0,0 +1,13 @@
+package com.bytebrawlers.ByteBrawlers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByteBrawlersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ByteBrawlersApplication.class, args);
+	}
+
+}
---
+++ main.js
@@ -0,0 +1,43 @@
+const search = document.querySelector(".navBar .search");
+const bannerSlideRight = document.querySelector(".banner-display button#right");
+const bannerSlideLeft = document.querySelector(".banner-display button#left");
+const featuredSlideRight = document.querySelector(".item-list button#right");
+const featuredSlideLeft = document.querySelector(".item-list button#left");
+const listItems = [...document.querySelector(".lists").children];
+const productCards = document.querySelectorAll(".card");
+
+productCards.forEach((card, index) => {
+  card.addEventListener("click", () => {
+    const productId = index + 1;
+    window.location.href = 'product.html?productId=${productId}';
+
+  })
+})
+
+function ItemCard({}) {}
+let currentList = 0;
+let previousList = 0;
+featuredSlideRight.addEventListener("click", () => {
+  previousList = currentList;
+  currentList = currentList < listItems.length - 1 ? currentList + 1 : 0;
+  update();
+});
+
+function update() {
+  listItems.forEach((item, index) => {
+    if (index === currentList) {
+      item.style.display = "grid";
+      item.classList.add("slide-in-left");
+    } else if (index === previousList) {
+      item.classList.add("slide-out-right");
+      item.style.transform = "translateY(100%)";
+      setTimeout(() => {
+        item.style.display = "none";
+      }, 800);
+      item.classList.remove("slide-in-left");
+    } else {
+      item.style.display = "none";
+      item.classList.remove("slide-out-right");
+    }
+  });
+}
