891ddcd5ad3a6269d0715bbef70c0b18df8dc953
Lukas <luetack@gmail.com>
2023-10-22 22:13:26 -0400

    Implementing SpringBoot, React, and Heroku

---
+++ src/main/java/com/example/ByteBrawlers/Controller/ClientForwardController.java
@@ -0,0 +1,13 @@
+package com.example.ByteBrawlers.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ClientForwardController {
+
+    @GetMapping(value="/hello")
+    public String forward() {
+        return "forward:/";
+    }
+}
---
+++ src/test/java/com/example/ByteBrawlers/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ByteBrawlers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
