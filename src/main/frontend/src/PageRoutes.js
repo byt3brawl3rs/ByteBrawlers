c70a2cc384a371b2a8ecc90953d7d32de48904bd
zhaob15 <zhaob15@miami.edu>
2023-11-19 20:46:10 -0500

    Search

--- src/main/frontend/src/PageRoutes.js
+++ src/main/frontend/src/PageRoutes.js
@@ -4,15 +4,17 @@ import MainWebPage from "./pages/MainWebPage";
 import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
 import CartPage from "./pages/CartPage";
+import Search from "./pages/Search";
 
 export const PageRoutes = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<MainWebPage/>}/>
+                <Route path="/i" element={<MainWebPage/>}/>
                 <Route path="/login" element={<LoginPage/>}/>
                 <Route path="/signup" element={<SignUpPage/>}/>
                 <Route path="/cart" element={<CartPage/>}/>
+                <Route path="/" element={<Search/>}/>
             </Routes>
         </BrowserRouter>
     );
