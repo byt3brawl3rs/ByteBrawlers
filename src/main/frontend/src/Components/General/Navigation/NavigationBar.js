170c2c9d1033b03c8c148cd951a8c6c0d6de5263
Jordan Hamlett <hamletje@miamioh.edu>
2023-12-04 23:10:35 -0500

    Logout?

--- src/main/frontend/src/Components/General/Navigation/NavigationBar.js
+++ src/main/frontend/src/Components/General/Navigation/NavigationBar.js
@@ -1,32 +1,52 @@
-import React, {Component} from "react";
+import React from "react";
+import { useNavigate } from "react-router-dom";
 import SearchBar from "./SearchBar";
 
 import "./CSS/NavigationBar.css";
 
-function NavigationBar() {
-    if (localStorage.getItem("isLoggedIn") === 'true') {
-        return (
-            <div className="NavigationBar">
-                <SearchBar/>
-                <a href="/">Home</a>
-                <a href="">About</a>
-                <a style={{float: "right"}} href="/profile">Profile</a>
-                <a style={{float: "right"}} href="/cart">Cart</a>
-            </div>
-        );
-    }
-    // const navigationBar = ;
-    return (
-        <div className="NavigationBar">
-            <SearchBar/>
-            <a href="/">Home</a>
-            <a href="">About</a>
-            <a style={{float: "right"}} href="/signup">SignUp</a>
-            <a style={{float: "right"}} href="/login">Login</a>
-            <a style={{float: "right"}} href="/cart">Cart</a>
-        </div>
-    );
+const NavigationBar = () => {
+  const navigate = useNavigate();
 
-}
+  const handleLogout = () => {
+    localStorage.setItem("isLoggedIn", "false");
+    localStorage.clear();
+    navigate("/");
+  };
+
+  const isLoggedIn = localStorage.getItem("isLoggedIn") === "true";
+
+  return (
+    <div className="NavigationBar">
+      <SearchBar />
+      <a href="/">Home</a>
+      <a href="">About</a>
+      {isLoggedIn ? (
+        <>
+          <a style={{ float: "right" }} href="/profile">
+            Profile
+          </a>
+          <a style={{ float: "right" }} href="/cart">
+            Cart
+          </a>
+          <a style={{ float: "right" }} onClick={handleLogout} href="/">
+            Logout
+          </a>
+        </>
+      ) : (
+        <>
+          <a style={{ float: "right" }} href="/signup">
+            SignUp
+          </a>
+          <a style={{ float: "right" }} href="/login">
+            Login
+          </a>
+          <a style={{ float: "right" }} href="/cart">
+            Cart
+          </a>
+        </>
+      )}
+    </div>
+  );
+};
 
 export default NavigationBar;
