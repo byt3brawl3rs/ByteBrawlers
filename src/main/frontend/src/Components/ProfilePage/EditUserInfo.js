4bf12490379c164daaf3dd50cc00b82279550b20
Jordan Hamlett <hamletje@miamioh.edu>
2023-12-04 22:29:16 -0500

    Profile Page Edits

--- src/main/frontend/src/Components/ProfilePage/EditUserInfo.js
+++ src/main/frontend/src/Components/ProfilePage/EditUserInfo.js
@@ -1,6 +1,7 @@
 // EditUserInfo.js
 
 import React, { useState } from 'react';
+import './CSS/EditUserInfo.css'; // Import the CSS file for styling
 
 const EditUserInfo = ({ user, onSave }) => {
   const [editedUser, setEditedUser] = useState({ ...user });
@@ -15,19 +16,34 @@ const EditUserInfo = ({ user, onSave }) => {
   };
 
   return (
-    <div>
+    <div className="edit-user-container">
       <h2>Edit User Profile</h2>
-      <label>
-        Name:
-        <input type="text" name="name" value={editedUser.name} onChange={handleChange} />
-      </label>
-      <label>
-        Email:
-        <input type="text" name="email" value={editedUser.email} onChange={handleChange} />
-      </label>
-      {/* Add other input fields for editing user information as needed */}
-
-      <button onClick={handleSave}>Save</button>
+      <form className="edit-form">
+        <label className="edit-label">
+          Name:
+          <input type="text" name="name" value={editedUser.name} onChange={handleChange} />
+        </label>
+        <label className="edit-label">
+          Email:
+          <input type="text" name="email" value={editedUser.email} onChange={handleChange} />
+        </label>
+        <label className="edit-label">
+          Username:
+          <input type="text" name="username" value={editedUser.username} onChange={handleChange} />
+        </label>
+        <label className="edit-label">
+          Phone:
+          <input type="text" name="phone" value={editedUser.phone} onChange={handleChange} />
+        </label>
+        <label className="edit-label">
+          Address:
+          <input type="text" name="address" value={editedUser.address} onChange={handleChange} />
+        </label>
+        {/* Add other input fields for editing user information as needed */}
+        <button className="save-button" onClick={handleSave}>
+          Save
+        </button>
+      </form>
     </div>
   );
 };
--- src/main/frontend/src/Components/ProfilePage/UserInfo.js
+++ src/main/frontend/src/Components/ProfilePage/UserInfo.js
@@ -1,16 +1,33 @@
 // UserInfo.js
 
 import React from 'react';
+import './CSS/UserInfo.css'; // Import a separate CSS file for styling
 
 const UserInfo = ({ user, onEdit }) => {
   return (
-    <div>
+    <div className="user-info-container">
       <h2>User Profile</h2>
-      <p>Name: {user.name}</p>
-      <p>Email: {user.email}</p>
-      {/* Add other user information as needed */}
-
-      <button onClick={onEdit}>Edit Profile</button>
+      <div className="user-info">
+        <p className="info-item">
+          <span className="info-label">Name:</span> {user.name}
+        </p>
+        <p className="info-item">
+          <span className="info-label">Email:</span> {user.email}
+        </p>
+        <p className="info-item">
+          <span className="info-label">Username:</span> {user.username}
+        </p>
+        <p className="info-item">
+          <span className="info-label">Phone:</span> {user.phone}
+        </p>
+        <p className="info-item">
+          <span className="info-label">Address:</span> {user.address}
+        </p>
+        {/* Add other user information as needed */}
+      </div>
+      <button className="edit-button" onClick={onEdit}>
+        Edit Profile
+      </button>
     </div>
   );
 };
